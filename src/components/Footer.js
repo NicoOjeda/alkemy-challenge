eecd5dc4b4be79e6dad68a4f73ef93e992878eb9
Nico ojeda <nico_ojeda86@hotmail.com>
2023-01-20 23:42:22 -0300

    se define footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <ul>
+            <li><a href="http://instagram.com" rel="noopener norefer">Instagram</a></li>
+        </ul>
+        <p>Copyright Alkemy Challenge</p>
+    </footer>
+  )
+}
+
+export default Footer;
