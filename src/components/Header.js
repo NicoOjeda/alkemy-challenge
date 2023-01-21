7d2b9c3dae5587db38e0a2d395d667bbebb2bac3
Nico ojeda <nico_ojeda86@hotmail.com>
2023-01-20 23:42:37 -0300

    se define header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+            <li>
+            <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/">Listado</Link>
+            </li>
+            <li>Contacto</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default  Header;
