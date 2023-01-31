51c747d73356b8fdfeecbcb55e8b3bb98d43f53a
Nico ojeda <nico_ojeda86@hotmail.com>
2023-01-30 23:37:03 -0300

    se agregan header y footer a app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Login from "./components/Login";
 import Listado from "./components/Listado";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Login/>}/>
       <Route path="/listado" element={<Listado/>}/>
     </Routes>
+    <Footer/>
    </>
    
   );
