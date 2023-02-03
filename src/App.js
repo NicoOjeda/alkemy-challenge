5d93e0112e67f4e0c8b49e9d447b208e4f09269f
Nico ojeda <nico_ojeda86@hotmail.com>
2023-02-03 19:36:36 -0300

    se agrega ruta de detalle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./components/Login";
 import Listado from "./components/Listado";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Detalle from "./components/Detalle";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login/>}/>
           <Route path="/listado" element={<Listado/>}/>
+          <Route path="/detalle" element={<Detalle/>}/>
         </Routes>
       </div>
       <Footer/>
